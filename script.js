edad37e2ac2921c91da20b07c0c72e3afe89b217
Katja Roehlig <katja-roehlig@mail.de>
2022-10-31 14:16:54 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const { createApp } = Vue;
+Vue.createApp({
+  data() {
+    return {
+      content: "password",
+      password1: "",
+      password2: "",
+      display: "",
+      conditions: {
+        equal: false,
+        loweCase: false,
+        upperCase: false,
+        numbers: false,
+        length: false,
+      },
+    };
+  },
+  methods: {
+    passwordConditions() {
+      if (this.password1 === this.password2) {
+        this.conditions.equal = true;
+      } else {
+        this.conditions.equal = false;
+      }
+      if (this.password1.match(/[a-z]/)) {
+        this.conditions.lowerCase = true;
+      } else {
+        this.conditions.lowerCase = false;
+      }
+      if (this.password1.match(/[A-Z]/)) {
+        this.conditions.upperCase = true;
+      } else {
+        this.conditions.upperCase = false;
+      }
+      if (this.password1.match(/\d/)) {
+        this.conditions.numbers = true;
+      } else {
+        this.conditions.numbers = false;
+      }
+      if (this.password1.length >= 10) {
+        this.conditions.length = true;
+      } else {
+        this.conditions.length = false;
+      }
+    },
+  },
+  /*computed: {
+    displayedText() {
+      if (this.conditions.equal) {
+        return "Passwords are equal ✅";;
+      } else {
+        return "Passwords are not equal ❌";
+      }
+    },*/
+  /*toggleView() {
+      if (this.content === "password") {
+        this.content = "text";
+      } else {
+        this.content = "password";
+      }
+    },*/
+}).mount("#app");
